44cf49803df1f36200fa6c1b43836c28b2aea742
bezliki <alya.serebro@bk.ru>
2025-11-03 13:40:21 +0300

    fix: update logs

--- lecture1/eventLoop/eventLoop2.js
+++ lecture1/eventLoop/eventLoop2.js
@@ -1,33 +1,33 @@
 const fs = require("fs");
 
-console.log("START");
+console.log("START"); //1
 
 setTimeout(() => {
-  setImmediate(() => console.log("add setImmediate"));
+  setImmediate(() => console.log("add setImmediate"));//7
 
-  console.log("setTimeout 1");
+  console.log("setTimeout 1"); //4
 }, 0);
 
-setImmediate(() => console.log("setImmediate"));
+setImmediate(() => console.log("setImmediate")); //6
 
-process.nextTick(() => console.log("Next Tick"));
+process.nextTick(() => console.log("Next Tick")); //3
 
-setTimeout(() => console.log("setTimeout 2"), 0);
+setTimeout(() => console.log("setTimeout 2"), 0); //5
 
 fs.readFile(__filename, () => {
-  setTimeout(() => console.log("readFile setTimeout"), 0);
-  setImmediate(() => console.log("readFile setImmediate"));
-  process.nextTick(() => console.log("readFile Next Tick"));
+  setTimeout(() => console.log("readFile setTimeout"), 0);//10
+  setImmediate(() => console.log("readFile setImmediate"));//9
+  process.nextTick(() => console.log("readFile Next Tick"));//8
 });
 
-console.log("END");
+console.log("END");//2
 
-//start
-//  end
-//  Next Tick
-// setImmediate
+// START
+// END
+// Next Tick
 // setTimeout 1
 // setTimeout 2
+// setImmediate
 // add setImmediate
 // readFile Next Tick
 // readFile setImmediate
